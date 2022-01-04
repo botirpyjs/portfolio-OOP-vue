96d99126db80aa2e075f0f57d53618ef9664ce62
Botir Akhmedov <95535127+botirpyjs@users.noreply.github.com>
2022-01-04 16:31:05 +0300

    vuejs and oop

---
+++ src/App.vue
@@ -0,0 +1,42 @@
+<template>
+    <div>
+        <comment-form @addComment="createComment" />
+        <comment-list v-bind:comments="comments" />            
+    </div>
+</template>
+<script>
+import CommentList from './components/CommentList';
+import CommentForm from './components/CommentForm';
+
+export default {
+    components:{
+        CommentList,
+        CommentForm
+    },
+
+    data() {
+        return {
+            comments: [
+                {id: 1, name: "Botir Akhmedov", email: "botircoders@gmail.com", content: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aspernatur, voluptatum."},
+                {id: 2, name: "Aziza Haydarova", email: "haydarova@gmail.com", content: "Lorem ipsum dolor, consectetur adipisicing elit. Aspernatur, voluptatum."},
+                {id: 3, name: "Davron Kamolov", email: "khamolov@gmail.com", content: "Lorem ipsum dolor, sit amet adipisicing elit. Aspernatur, voluptatum."}
+            ],
+            name: "",
+            email: "",
+            content: "",
+        }
+    },
+        methods: {
+            createComment(comment) {
+                this.comments.push(comment);
+            }
+        },
+}
+</script>
+<style>
+.comment{
+    padding: 10px;
+    margin: 10px;
+    border: 3px solid #ccc;
+}
+</style>
---
+++ src/components/CommentForm.vue
@@ -0,0 +1,42 @@
+<template>
+    <div>
+        <div class="w-75 mx-auto">
+            <h1 class="text-center">Add comment</h1>
+            <form @submit.prevent>
+                <input v-model="comment.name" type="text" placeholder="Enter your name..." class="form-control">
+                <input v-model="comment.email" type="email" placeholder="Enter your email..." class="form-control my-2">
+                <input v-model="comment.content" type="text" placeholder="Enter your content..." class="form-control">
+                <div class="d-flex flex-row-reverse">
+                    <button @click="createComment" class="btn btn-danger mt-3 w-25" >Add commit</button>
+                </div>
+            </form>
+        </div>        
+    </div>
+</template>
+<script>
+export default {
+    data() {
+        return {
+            comment: {
+                name: '',
+                email: '',
+                content: ''
+            }
+        }
+    },
+    methods: {
+        createComment() {
+            this.comment.id = Date.now();
+            this.$emit('addComment', this.comment);
+            this.comment = {
+                name: '',
+                email: '',
+                content: ''
+            }
+        }
+    },
+}
+</script>
+<style>
+
+</style>
